691ab5d7e043eecaa5cb772d72b36ad6c680e22f
Mehedi86 <abdullahalmehedi@gmail.com>
2025-01-23 12:44:30 +0600

    first commit

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,14 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+const MainLayout = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <div className="min-h-[calc(100vh-232px)]"></div>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default MainLayout;
